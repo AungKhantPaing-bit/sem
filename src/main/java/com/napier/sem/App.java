2cda41eb10677d7e02e9b5b36a8f5656594d0ed6
AungKhantPaing-bit <73480276+aungkhantpaing-bit@users.noreply.github.com>
2020-11-18 15:06:06 +0630

    close the output

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -13,9 +13,9 @@ public class App
         // Connect to database
         a.connect();
         // Get Employee
-        Employee emp = a.getEmployee(255530);
+//        Employee emp = a.getEmployee(255530);
         // Display results
-        a.displayEmployee(emp);
+//        a.displayEmployee(emp);
         // Disconnect from database
         a.disconnect();
     }
