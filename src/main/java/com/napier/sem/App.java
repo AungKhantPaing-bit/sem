e0da75e51a766ba431168b73459386acb278f4ee
40478639 <40478639@live.napier.ac.uk>
2020-11-19 11:45:55 +0630

    change the travis.yml

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -12,17 +12,17 @@ public class App
         App a = new App();
 
         // Connect to database
-        a.connect("localhost:33060");
+        a.connect("localhost:33066");
         // Get Employee
-//        Employee emp = a.getEmployee(255530);
+        Employee emp = a.getEmployee(255530);
         // Display results
-//        a.displayEmployee(emp);
+        a.displayEmployee(emp);
 
         // Extract employee salary information
-//        ArrayList<Employee> employees = a.getAllSalaries();
+        ArrayList<Employee> employees = a.getAllSalaries();
 
         // output the array list
-//        a.printSalaries(employees);
+        a.printSalaries(employees);
         // Disconnect from database
         a.disconnect();
     }
